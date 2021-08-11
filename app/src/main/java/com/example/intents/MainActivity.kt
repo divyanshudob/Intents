614f0cfdcfe7523254240647706737f681132ec2
Divyanshu Dobriyal <divyanshudob@gmail.com>
2021-08-11 12:37:49 +0530

    Email Intent Completed

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.intents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import javax.security.auth.Subject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var send : Button = findViewById(R.id.Send)
+
+        send.setOnClickListener{
+            var email: EditText = findViewById(R.id.Email_id)
+            var subject: EditText = findViewById(R.id.Subject)
+            var body: EditText = findViewById(R.id.Body)
+
+            var EMAIL = email.text.toString().trim()
+            var SUBJECT = subject.text.toString().trim()
+            var BODY = body.text.toString().trim()
+
+            var inTent = Intent(Intent.ACTION_SEND)
+            inTent.data = Uri.parse("mailto:")
+            inTent.type ="text/plain"
+
+            inTent.putExtra(Intent.EXTRA_EMAIL,arrayOf(EMAIL))
+            inTent.putExtra(Intent.EXTRA_SUBJECT,SUBJECT)
+            inTent.putExtra(Intent.EXTRA_TEXT,BODY)
+
+
+           startActivity(inTent)
+
+        }
     }
 }
