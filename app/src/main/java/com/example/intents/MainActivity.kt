f730d89cdf6a35ce56d8448bdb95b196c6c8cff7
Divyanshu Dobriyal <divyanshudob@gmail.com>
2021-08-11 12:55:53 +0530

    Call Intent Completed

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.intents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var call: Button = findViewById(R.id.Call)
+
+        call.setOnClickListener{
+           var eTv: EditText = findViewById(R.id.eTv)
+           var number = eTv.text.toString()
+
+           var inTent = Intent(Intent.ACTION_CALL)
+           inTent.data = Uri.parse("tel:" + number)
+            startActivity(inTent)
+
+        }
     }
 }
